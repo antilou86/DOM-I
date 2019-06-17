ab255347f1cffaf7041ff54cd7aa092a378a68c0
Lou <antilou86@gmail.com>
2019-06-17 15:37:31 -0400

    WIP: wrote a for loop.

--- js/index.js
+++ js/index.js
@@ -40,3 +40,13 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let navi = document.querySelectorAll('nav a');
+
+for (let i=0; i <= navi.length; i++) {
+  navi[i].textContent = siteContent.nav[`nav-item-${i+1}`]; 
+}
+
+
+
+
